5f26b526c57ae84baf6a20d0bc10f299fa3bc877
0xEvilTwin <noname.rock.gt@gmail.com>
2013-01-23 19:06:58 +0700

    Update Assignment13/Assignment13/Date.h

---
+++ Assignment13/Assignment13/Date.h
@@ -0,0 +1,16 @@
+#ifndef _Date_h
+#define _Date_h
+#include <string>
+
+class Date {
+ public:
+ 
+private:
+  int date = 1;
+  enum Month{
+    January = 1,FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, NOVEMBER, DECEMBER
+  };
+	int year = 2000;
+
+}
+#endif
