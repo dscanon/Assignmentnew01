4cb368aadf4e49e538eecc6c184448a05a87f767
0xEvilTwin <noname.rock.gt@gmail.com>
2013-01-27 06:39:03 +0700

    Update Assignment13/Assignment13/Date.cpp

--- Assignment13/Assignment13/Date.cpp
+++ Assignment13/Assignment13/Date.cpp
@@ -52,13 +52,14 @@ string Date::monthToString(Month month){
 		case JULY: return "July";
 		case AUGUST: return "August";
 		case SEPTEMBER: return "September";
+		case OCTOBER: return "October";
 		case NOVEMBER: return "November";
 		case DECEMBER: return "December";
 		default: return "";
 	}
 }
 
-int Date::daysInMonth(Month month, int year){
+int Date::daysInMonth(Month month,int year){
 	return month==2?(28+isLeapYear(year)):31-(month-1)%7%2;		
 }
 
@@ -98,41 +99,132 @@ bool operator>(Date d1, Date d2){
 bool operator>=(Date d1, Date d2){
 	return d1 > d2 || d1==d2;
 }
+
 Date operator+(Date d1, int n){
-	if(d1.day+n<=daysInMonth(d1.month))
-	{
-		d1.day += n;
-		return d1;
-	}
-	else{	
-		int m=d1.day+n;
-		while(m>daysInMonth(d1.month)){
-			m=m-dayInMonth(d1.month);
-			Month(d1.month++);
-  				while(d1.month>12){
-				d1.month=1;
-				year++;	
-				}	
-		}	
-	}		
-}
-Date operator-(Date d1, int n){
-	if(n<d1.day){
-		d1.day = d1.day-n;
-		return d1;
-	}
-	else{	
-		int m=n-d1.day;
-		while(m>0){
-			d1.month--;
-			while(m>daysInMonth(month)){
-				m = m-daysInMonth(month);
-				month--;
-			}
-			d1.day = daysInMonth(month)-m;
-			return d1;
+	d1.day += n;
+	while(d1.day > Date::daysInMonth(d1.month,d1.year)){
+		d1.day -= Date::daysInMonth(d1.month,d1.year);
+		(Month)d1.month = Month(d1.month+1);
+		if(d1.month > 12){
+			d1.month = Month(1);
+			d1.year++;
 		}
-		
 	}
+	return d1;
+}
+
+Date operator-(Date d1, int n){
+	d1.day -= n;
+	while(d1.day<=0){
+		d1.month = Month (d1.month-1);
+		if(d1.month < 1){
+			d1.month = Month(12);
+			d1.year--;
+		}
+		d1.day = Date::daysInMonth(d1.month, d1.year)+d1.day;
+	}
+	
+	return d1;
+}
+
+
+
+int Date::dayPast(){
+	int dayPast=0;
+	Month mon = JANUARY;
+	while(this->month != mon){
+		dayPast += Date::daysInMonth(mon,this->year);
+		mon = Month (mon+1);
+	}
+	dayPast += this->day;
+	return dayPast;
+}
+int Date::dayLeft(){
+	Month mon = DECEMBER;
+	int dayLeft=0;
+	while(this->month != mon){
+		dayLeft += Date::daysInMonth(mon,this->year);
+		mon = Month (mon-1);
+	}
+
+	dayLeft+= Date::daysInMonth(mon,this->year)- this->day;
+	return dayLeft;
+}
+
+int Date::dayBetweenYear(Date d2){
+	int days=0;
+	for(int i = (this->year)+1 ; i<d2.year ; i++){
+		days += Date::isLeapYear(i)?366:365;
+	}
+	return days;
+}
+
+int operator-(Date d1, Date d2){
+	int diffDay = 0;
+	if (d1 == d2) return diffDay;
+	
+	else if(d1 > d2){
+		diffDay += d1.dayBetweenYear(d2);
+		if(d1.year == d2.year) diffDay +=  (d1.dayPast() - ((Date::isLeapYear(d1.year)?366:365)-d2.dayLeft()));
+		else diffDay +=  (d1.dayPast() + d2.dayLeft());
+	}
+
+	else{
+		diffDay += d1.dayBetweenYear(d2);
+		if(d2.year == d1.year) diffDay +=  (d2.dayPast() - ((Date::isLeapYear(d2.year)?366:365)-d1.dayLeft()));
+		else diffDay +=  (d2.dayPast() + d1.dayLeft());
+	}
+	return diffDay;
+
+}
+Date & Date::operator=(Date d1){
+	this->day = d1.day;
+	this->month = d1.month;
+	this->year = d1.year;
+	return *this;
+}
+Date & Date::operator+=(int n){
+	day += n;
+	while(day > Date::daysInMonth(month,year)){
+		day -= Date::daysInMonth(month,year);
+		(Month)month = Month(month+1);
+		if(month > 12){
+			month = Month(1);
+			year++;
+		}
+	}
+	return *this;
+}
+Date & Date::operator-=(int n){
+	day -= n;
+	while(day<=0){
+		month = Month (month-1);
+		if(month < 1){
+			month = Month(12);
+			year--;
+		}
+		day = Date::daysInMonth(month, year)+day;
+	}
+	
+	return *this;
+}
+
+Date operator++(Date &d1){
+	d1+=1;
+	return d1;
+}
+Date operator++(Date &d1, int x){
+	Date old = d1;
+	d1+=1;
+	return old;
+}
+Date operator--(Date &d1){
+	d1-=1;
+	return d1;
+}
+Date operator--(Date &d1, int){
+	Date old = d1;
+	d1-=1;
+	return old;
 	
 }
